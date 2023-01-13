b55e0a661214c891236499721794e2abd4ea5f75
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2023-01-13 19:15:58 +0530

    build the basic frontend and lay out basic routing

--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-	return (
-		<>
-			<h1>Welcome to Proshop</h1>
-		</>
-	);
-}
-
-export default App;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Container, Row, Col } from "react-bootstrap";
+function Footer() {
+	return (
+		<footer>
+			<Row>
+				<Col className="text-center py-3">Copyright &copy; ProShop</Col>
+			</Row>
+		</footer>
+	);
+}
+export default Footer;
